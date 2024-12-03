5ccc4a1a187a4ea39ea197ef83ad836d3d754740
Emma Boyd <emmanb33@gmail.com>
2024-12-02 21:22:43 -0600

    Create Recipe.js

---
+++ server/models/Recipe.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    ingredients: { type: [String], required: true },
+    instructions: { type: String, required: true },
+}, { timestamps: true });
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+module.exports = Recipe;
