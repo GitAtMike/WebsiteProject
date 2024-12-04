d38f42d3b36ba7a3d7476bb9f873f5069e6810f5
Emma Boyd <emmanb33@gmail.com>
2024-12-04 17:58:17 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,56 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require ('body-parser');
 const cors = require('cors');
+const bodyParser = require ('body-parser');
+require("dotenv").config();
+
+const app = express();
+cost PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser: true;
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+app.get("/recipes", async (req, res) => {
+  try{
+    const recipes = await Recipe.find();
+    res.json(recipes);
+  }
+  catch(err){
+    res.status(500).json({error: "Failed to fetch recipes"});
+  }
+});
+
+app.post("/recipes", async (req, res) => {
+  try {
+    const { title, description, ingredients, instructions } = req.body;
+    const newRecipe = new Recipe({ title, description, ingredients, instructions });
+    await newRecipe.save();
+    res.status(201).json(newRecipe);
+  } 
+  catch (err) {
+    res.status(400).json({ error: "Failed to add recipe" });
+  }
+});
+
+app.delete("/recipes", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Recipe.findByIdAndDelete(id);
+    res.status(200).json({ message: "Recipe deleted successfully" });
+  } 
+  catch (err) {
+    res.status(500).json({ error: "Failed to delete recipe" });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
